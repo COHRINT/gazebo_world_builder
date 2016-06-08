65b33cf474846cb9d42ddc604c3d9c9c305b68f1
Brett Israelsen <brett.israelsen@gmail.com>
2016-06-08 15:57:04 -0600

    increase resolution of pbm, automatically produce 90deg rotation as well

--- bin/roadnetgen.py
+++ bin/roadnetgen.py
@@ -3,10 +3,11 @@ import numpy
 from numpy.random import random_integers as rand
 import matplotlib.pyplot as pyplot
 import networkx as nx
+import scipy.misc as sm
 from toPBM import writePBM
 
 
-def roadNetGen(roadnet):
+def roadNetGen(roadnet,sf=1):
     # TODO: This is pretty hacked together. Make this better
     try:
         # iterate over neighbors
@@ -39,11 +40,13 @@ def roadNetGen(roadnet):
 
     #Z[height/2-edg[2]:height/2+edg[0],width/2]=0
     #Z[height/2, width/2-edg[3]:width/2+edg[1]]=0
-    print(sze/2,sze/2+1)
-    Z[sze/2-edg[2]:sze/2+edg[0],sze/2:sze/2+2]=0
-    Z[sze/2:sze/2+2 , sze/2-edg[3]:sze/2+edg[1]]  = 0
+    Z[sze/2-edg[2]:sze/2+edg[0], sze/2:sze/2+2] = 0
+    Z[sze/2:sze/2+2, sze/2-edg[3]:sze/2+edg[1]]  = 0
 
-    return Z
+    Zfinal = sm.imresize(Z, sf*100, interp='nearest')
+    Zfinal[Zfinal>0] = 1
+
+    return Zfinal
 
 # Build a road network
 RN = nx.Graph()
@@ -53,13 +56,16 @@ RN.add_edge(0, 3, weight=5)
 RN.add_edge(0, 4, weight=3)
 
 # Generate the occupancy grid
-roadnet_mat = numpy.rot90(roadNetGen(RN))
+
+roadnet_mat = roadNetGen(RN,sf=10)
+roadnet_mat_90 = numpy.rot90(roadnet_mat)
 
 pyplot.figure(figsize=(10, 5))
 # pyplot.imshow(maze(80, 40), cmap=pyplot.cm.binary, interpolation='nearest')
 # genMaze = maze(31,31, complexity=0.1, density=0.15)
 
 writePBM(roadnet_mat,name="roadnet")
+writePBM(roadnet_mat_90,name="roadnet_rot90")
 
 pyplot.imshow(roadnet_mat, cmap=pyplot.cm.binary, interpolation='nearest')
 pyplot.xticks([]), pyplot.yticks([])
